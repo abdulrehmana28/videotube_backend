7b3690543f817c79c87552c6c9f791999ae79402
Abdulrehman Azam <abdulrehmana28x@gmail.com>
2025-08-08 15:25:35 +0500

    Add controllers and routes TODOs
    Introduces initial controller and route files for comments, dashboard, likes, playlists, subscriptions, tweets, and videos. Sets up Express routes with JWT authentication middleware and stubs for CRUD operations and feature toggles. This scaffolds the backend structure for core video platform functionalities.

---
+++ src/routes/tweet.routes.js
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+import {
+    createTweet,
+    deleteTweet,
+    getUserTweets,
+    updateTweet,
+} from "../controllers/tweet.controller.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js"
+
+const router = Router();
+
+router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+
+router.post("/", createTweet);
+
+router.get("/user/:userId", getUserTweets);
+
+router.route("/:tweetId")
+    .patch(updateTweet)
+    .delete(deleteTweet);
+
+// router.patch("/:tweetId", updateTweet);
+// router.delete("/:tweetId", deleteTweet);
+export default router
